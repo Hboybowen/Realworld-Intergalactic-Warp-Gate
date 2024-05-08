55f52e0abe4bad81820f913300678dea21a63827
Hugh Bowen <hbowen2965@hotmail.com>
2024-05-08 16:44:44 -0500

    Added

---
+++ MathematicFirmsofMemphis_Warpgates.cs
@@ -0,0 +1,611 @@
+﻿using System;
+using System.IO;
+
+public class WarpGate
+{
+    // Existing code...
+
+    private bool isMetalDetected; // Added metal detection field
+    public string ModelNumber { get; set; } // Added model number property
+    public bool IsProbeEnabledAlways { get; set; } // Added probe enabled property
+
+    public void MoveMetalAtMaxSpeed()
+    {
+        while (isMetalDetected)
+        {
+            // Move metal at max speed
+            // ...
+            // Check if further motion is sensed by sensors
+            if (!isMotionDetectorActivated)
+            {
+                isMetalDetected = false;
+            }
+        }
+    }
+
+    public void AddMagneticField()
+    {
+        // Create gravitational forces
+        // ...
+    }
+
+    private void FireSequence(Vector3 firingVector)
+    {
+        // Perform firing sequence using the given vector
+        // ...
+    }
+
+    public void AddHighPoweredMagnet(isHighPoweredMagnetdetected)
+    {
+        // Add high powered magnet to motherboard
+        // ...
+    }
+
+    public void AddCeramic(isCeramicDetected)
+    {
+        // Add ceramic
+        // ...
+        FireSequence();
+    }
+
+    public void AddCarbon(isCarbonDetected)
+    {
+        // Add carbon
+        // ...
+        FireSequence();
+    }
+
+    public void AddPlastic(isPlasticDetected)
+    {
+        // Add plastic
+        // ...
+        FireSequence();
+    }
+
+    public void AddMetal(isMetalDetected)
+    {
+        // Add metal
+        // ...
+        FireSequence();
+    }
+
+    public void AddRock(isRockDetected)
+    {
+        // Add rock
+        // ...
+        FireSequence();
+    }
+
+    public void MetalDetectionAtZeroMeters()
+    {
+        if (isMotionDetectorActivated && isMetalDetected)
+        {
+            MoveMetalAtMaxSpeed();
+        }
+    }
+
+    // Rest of the code...
+
+    private void FireSequence()
+    {
+        // Perform firing sequence
+        // ...
+    }
+}
+
+public class using System;
+        using System.Collections.Generic;
+
+public class WarpGate
+{
+    // Existing code...
+
+    private bool isMetalDetected; // Added metal detection field
+    public string ModelNumber { get; set; } // Added model number property
+
+    public double Km { get; set; } // Added Km property
+
+    public void MoveMetalAtMaxSpeed()
+    {
+        while (isMetalDetected)
+        {
+            // Move metal at max speed
+            // ...
+            // Check if further motion is sensed by sensors
+            if (!isMotionDetectorActivated)
+            {
+                isMetalDetected = false;
+            }
+        }
+    }
+
+    public void AddMagneticField()
+    {
+        // Create gravitational forces
+        // ...
+    }
+
+    private void FireSequence(Vector3 firingVector)
+    {
+        // Perform firing sequence using the given vector
+        // ...
+    }
+
+    public void AddHighPoweredMagnet(isHighPoweredMagnetdetected)
+    {
+        // Add high powered magnet to motherboard
+        // ...
+    }
+
+    public void AddCeramic(isCeramicDetected)
+    {
+        // Add ceramic
+        // ...
+        FireSequence();
+    }
+
+    public void AddCarbon(isCarbonDetected)
+    {
+        // Add carbon
+        // ...
+        FireSequence();
+    }
+
+    public void AddPlastic(isPlasticDetected)
+    {
+        // Add plastic
+        // ...
+        FireSequence();
+    }
+
+    public void AddMetal(isMetalDetected)
+    {
+        // Add metal
+        // ...
+        FireSequence();
+    }
+
+    public void AddRock(isRockDetected)
+    {
+        // Add rock
+        // ...
+        FireSequence();
+    }
+
+    public void MetalDetectionAtZeroMeters()
+    {
+        if (isMotionDetectorActivated && isMetalDetected)
+        {
+            MoveMetalAtMaxSpeed();
+        }
+    }
+
+    // Rest of the code...
+
+    private void FireSequence()
+    {
+        // Perform firing sequence
+        // ...
+    }
+}
+
+public class WarpGateNetwork
+{
+    private List<WarpGate> warpgates;
+
+    public WarpGateNetwork()
+    {
+        warpgates = new List<WarpGate>();
+    }
+
+    public void AddWarpGate(WarpGate warpGate)
+    {
+        warpgates.Add(warpGate);
+    }
+
+    public void ConnectWarpGates(WarpGate warpGate1, WarpGate warpGate2)
+    {
+        warpGate1.AddConnectedWarpGate(warpGate2);
+        warpGate2.AddConnectedWarpGate(warpGate1);
+    }
+
+    public void CreatePlotOfWarpgates(int power)
+    {
+        int numWarpgates = (int)Math.Pow(2, power);
+
+        for (int i = 0; i < numWarpgates; i++)
+            public void ConnectWarpGatesByMagneticField(WarpGate warpGate1, WarpGate warpGate2)
+            {
+                warpGate1.AddMagneticField();
+                warpGate2.AddMagneticField();
+
+                Vector3 gravitationalForce = CalculateGravitationalForce(warpGate1, warpGate2);
+                warpGate1.FireSequence(gravitationalForce);
+                warpGate2.FireSequence(gravitationalForce);
+
+                warpGate1.MetalDetectionAtZeroMeters();
+                warpGate2.MetalDetectionAtZeroMeters();
+            }
+
+        private Vector3 CalculateGravitationalForce(WarpGate warpGate1, WarpGate warpGate2)
+        {
+            // Calculate gravitational force based on the warp gate positions
+            // ...
+            return gravitationalForce;
+        }
+
+        public void MetalDetectionAtZeroMeters()
+        {
+            if (isMotionDetectorActivated && isMetalDetected)
+            {
+                MoveMetalAtMaxSpeed();
+                ReactivateSensors();
+            }
+        }
+
+        private void ReactivateSensors()
+        {
+            // Reactivate sensors to check if metal is detected again
+            // ...
+            while (!isMetalDetected)
+            {
+                // Check if metal is detected
+                // ...
+                if (isMetalDetected)
+                {
+                    MetalDetectionAtZeroMeters();
+                }
+                else
+                {
+                    DeactivateSensors();
+                }
+            }
+        }
+
+        private void DeactivateSensors()
+        {
+            // Deactivate sensors
+            // ...
+        }
+        {
+            WarpGate warpGate = new WarpGate();
+            AddWarpGate(warpGate);
+        }
+
+        double minDistance = Math.Pow(2, 2781636781606255);
+        double maxDistance = Math.Pow(2, 13871273817280978102344424223788563121);
+
+        for (int i = 0; i < numWarpgates - 1; i++)
+        {
+            double distance = GetRandomDistance(minDistance, maxDistance);
+            ConnectWarpGates(warpgates[i], warpgates[i + 1], distance);
+        }
+
+        double lastDistance = GetRandomDistance(minDistance, maxDistance);
+        ConnectWarpGates(warpgates[numWarpgates - 1], warpgates[0], lastDistance);
+    }
+
+    private double GetRandomDistance(double minDistance, double maxDistance)
+    {
+        Random random = new Random();
+        return random.NextDouble() * (maxDistance - minDistance) + minDistance;
+    }
+}
+public class WarpGateNetwork2
+{
+    private List<WarpGate> warpgates;
+
+    public WarpGateNetwork()
+    {
+        warpgates = new List<WarpGate>();
+    }
+
+    public void AddWarpGate(WarpGate warpGate)
+    {
+        warpgates.Add(warpGate);
+    }
+
+    public void ConnectWarpGates(WarpGate warpGate1, WarpGate warpGate2)
+    {
+        warpGate1.AddConnectedWarpGate(warpGate2);
+        warpGate2.AddConnectedWarpGate(warpGate1);
+    }
+
+    public void CreatePlotOfWarpgates(int power)
+    {
+        int numWarpgates = (int)Math.Pow(2, power);
+
+        for (int i = 0; i < numWarpgates; i++)
+            public void ConnectWarpGatesByMagneticField(WarpGate warpGate1, WarpGate warpGate2)
+            {
+                warpGate1.AddMagneticField();
+                warpGate2.AddMagneticField();
+
+                Vector3 gravitationalForce = CalculateGravitationalForce(warpGate1, warpGate2);
+                warpGate1.FireSequence(gravitationalForce);
+                warpGate2.FireSequence(gravitationalForce);
+
+                warpGate1.MetalDetectionAtZeroMeters();
+                warpGate2.MetalDetectionAtZeroMeters();
+            }
+
+        private Vector3 CalculateGravitationalForce(WarpGate warpGate1, WarpGate warpGate2)
+        {
+            // Calculate gravitational force based on the warp gate positions
+            // ...
+            return gravitationalForce;
+        }
+
+        public void MetalDetectionAtZeroMeters()
+        {
+            if (isMotionDetectorActivated && isMetalDetected)
+            {
+                MoveMetalAtMaxSpeed();
+                ReactivateSensors();
+            }
+        }
+
+        private void ReactivateSensors()
+        {
+            // Reactivate sensors to check if metal is detected again
+            // ...
+            while (!isMetalDetected)
+            {
+                // Check if metal is detected
+                // ...
+                if (isMetalDetected)
+                {
+                    MetalDetectionAtZeroMeters();
+                }
+                else
+                {
+                    DeactivateSensors();
+                }
+            }
+        }
+
+        private void DeactivateSensors()
+        {
+            // Deactivate sensors
+            // ...
+        }
+        {
+            WarpGate warpGate = new WarpGate();
+            AddWarpGate(warpGate);
+        }
+
+        double minDistance = Math.Pow(2, 2781636781606255);
+        double maxDistance = Math.Pow(2, 13871273817280978102344424223788563121);
+
+        for (int i = 0; i < numWarpgates - 1; i++)
+        {
+            double distance = GetRandomDistance(minDistance, maxDistance);
+            ConnectWarpGates(warpgates[i], warpgates[i + 1], distance);
+        }
+
+        double lastDistance = GetRandomDistance(minDistance, maxDistance);
+        ConnectWarpGates(warpgates[numWarpgates - 1], warpgates[0], lastDistance);
+    }
+
+    private double GetRandomDistance(double minDistance, double maxDistance)
+    {
+        Random random = new Random();
+        return random.NextDouble() * (maxDistance - minDistance) + minDistance;
+    }
+}
+public class WarpGateNetwork3
+{
+    private List<WarpGate> warpgates;
+
+    public WarpGateNetwork()
+    {
+        warpgates = new List<WarpGate>();
+    }
+
+    public void AddWarpGate(WarpGate warpGate)
+    {
+        warpgates.Add(warpGate);
+    }
+
+    public void ConnectWarpGates(WarpGate warpGate1, WarpGate warpGate2)
+    {
+        warpGate1.AddConnectedWarpGate(warpGate2);
+        warpGate2.AddConnectedWarpGate(warpGate1);
+    }
+
+    public void CreatePlotOfWarpgates(int power)
+    {
+        int numWarpgates = (int)Math.Pow(2, power);
+
+        for (int i = 0; i < numWarpgates; i++)
+            public void ConnectWarpGatesByMagneticField(WarpGate warpGate1, WarpGate warpGate2)
+            {
+                warpGate1.AddMagneticField();
+                warpGate2.AddMagneticField();
+
+                Vector3 gravitationalForce = CalculateGravitationalForce(warpGate1, warpGate2);
+                warpGate1.FireSequence(gravitationalForce);
+                warpGate2.FireSequence(gravitationalForce);
+
+                warpGate1.MetalDetectionAtZeroMeters();
+                warpGate2.MetalDetectionAtZeroMeters();
+            }
+
+        private Vector3 CalculateGravitationalForce(WarpGate warpGate1, WarpGate warpGate2)
+        {
+            // Calculate gravitational force based on the warp gate positions
+            // ...
+            return gravitationalForce;
+        }
+
+        public void MetalDetectionAtZeroMeters()
+        {
+            if (isMotionDetectorActivated && isMetalDetected)
+            {
+                MoveMetalAtMaxSpeed();
+                ReactivateSensors();
+            }
+        }
+
+        private void ReactivateSensors()
+        {
+            // Reactivate sensors to check if metal is detected again
+            // ...
+            while (!isMetalDetected)
+            {
+                // Check if metal is detected
+                // ...
+                if (isMetalDetected)
+                {
+                    MetalDetectionAtZeroMeters();
+                }
+                else
+                {
+                    DeactivateSensors();
+                }
+            }
+        }
+
+        private void DeactivateSensors()
+        {
+            // Deactivate sensors
+            // ...
+        }
+        {
+            WarpGate warpGate = new WarpGate();
+            AddWarpGate(warpGate);
+        }
+
+        double minDistance = Math.Pow(2, 2781636781606255);
+        double maxDistance = Math.Pow(2, 13871273817280978102344424223788563121);
+
+        for (int i = 0; i < numWarpgates - 1; i++)
+        {
+            double distance = GetRandomDistance(minDistance, maxDistance);
+            ConnectWarpGates(warpgates[i], warpgates[i + 1], distance);
+        }
+
+        double lastDistance = GetRandomDistance(minDistance, maxDistance);
+        ConnectWarpGates(warpgates[numWarpgates - 1], warpgates[0], lastDistance);
+    }
+
+    private double GetRandomDistance(double minDistance, double maxDistance)
+    {
+        Random random = new Random();
+        return random.NextDouble() * (maxDistance - minDistance) + minDistance;
+    }
+}
+
+public void AddCarbon()
+    {
+        // Add carbon
+        // ...
+    }
+
+    public void AddPlastic()
+    {
+        // Add plastic
+        // ...
+    }
+public void AddCeramicAlloy()
+{
+    // Add ceramic alloy
+    // ...
+}
+
+public void MetalDetectionAtZeroMeters()
+    {
+        if (isMotionDetectorActivated && isMetalDetected)
+        {
+            MoveMetalAtMaxSpeed();
+        }
+        else
+        {
+            ReactivateSensors();
+        }
+    }
+
+    private void ReactivateSensors()
+    {
+        // Reactivate sensors to check if metal is detected again
+        // ...
+        while (!isMetalDetected)
+        {
+            // Check if metal is detected
+            // ...
+            if (isMetalDetected)
+            {
+                MetalDetectionAtZeroMeters();
+            }
+            else
+            {
+                DeactivateSensors();
+            }
+        }
+    }
+
+    private void DeactivateSensors()
+    {
+        // Deactivate sensors
+        // ...
+    }
+
+    // Rest of the code...
+}
+
+public class Magnet
+{
+    public double CalculateAcceleration()
+    {
+        double multiplier = Math.Pow(2, 10000000000000);
+        double acceleration = multiplier * 299792458; // Speed of light in meters per second squared
+        return acceleration;
+    }
+}
+    {
+        while (isMetalDetected)
+        {
+            // Move metal at max speed
+            // ...
+            // Check if further motion is sensed by sensors
+            if (!isMotionDetectorActivated)
+            {
+                isMetalDetected = false;
+            }
+        }
+    }
+
+    public void AddHighPoweredMagnet(using computer)
+    {
+        // Add high powered magnet to motherboard
+        // ...
+    }
+
+    public void AddCeramic()
+    {
+        // Add ceramic
+        // ...
+    }
+
+    public void AddCarbon()
+    {
+        // Add carbon
+        // ...
+    }
+
+    public void AddPlastic()
+    {
+        // Add plastic
+        // ...
+    }
+
+    public void MetalDetectionAtZeroMeters()
+    {
+        if (isMotionDetectorActivated && isMetalDetected)
+        {
+            MoveMetalAtMaxSpeed();
+        }
+    }
+
+    // Rest of the code...
+}
